556f0221187220560c2d1bbfbc9e04b9c574aacc
anniepod <31967599+anniepod@users.noreply.github.com>
2018-01-23 13:43:08 -0500

    HW UPDATE

--- two.py
+++ two.py
@@ -1,15 +1,3 @@
-"""
-You go on a wonderful holiday leaving on day number 3 (a Wednesday).
-You return home after 137 sleeps.
-
-Write a general version of the program which asks
-for the starting day number,
-and the length of your stay,
-`and it will tell you the name of day of the
-week you will return on.
-"""
-
-
 def day_num_to_name(num):
     if num == 0:
         return "Sunday"
@@ -33,5 +21,5 @@ def day_num_to_name(num):
 leave_day = int(input("What day are you leaving? (0-6)"))
 away_days = int(input("How many nights are you gone?"))
 
-day_back = (leave_day + away_days)# % 7 leaving this out for now - remember to put back
+day_back = ((leave_day + away_days)%7)# % 7 leaving this out for now - remember to put back
 print("You will return on a",day_num_to_name(day_back))
